e4b4283409ee6c9b55ce6f318e374ba30bf76aad
Aleksandra Przywrzej <przepjurka@gmail.com>
2021-10-04 12:14:54 +0200

    ostateczna wersja kalkulatora walut

--- app.py
+++ app.py
@@ -8,33 +8,20 @@ def get_rate(file_name, currency):
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             if row: 
-                if currency==row [1]:
+                if currency==row [0]:
                    return float(row[2])
-print(get_rate('data.csv', 'USD'))
+print(get_rate('data.csv', 'dolar amerykański'))
 
 @app.route('/', methods=['GET', 'POST']) 
-def currency():#nie wiem czy prawidłowo rozumiem tą funkcję:
-   if request.method == 'GET': #jeśli zapytanie request to get
-       return render_template ("form.html")#to zwraca formularz na stronie
-   elif request.method == 'POST':#jeśli zapytanie request to post to ##post nie działa
-       waluta=request.form('waluta')#pobieram z formularza wartość 'waluta'
-       ilość =request.form['ilość']#pobieram z formularza wartość 'ilość'
-       get_rate('data.csv', 'waluta')#odpalam funkcje wybierającą z pliku z api wybraną w formularzu walutę
-       wynik=float(ilość)*float(waluta)#przypisuję do zmiennej 'wynik' iloczyn floatów z 'waluty' i 'ilości'
-       return render_template('form.html', wynik=wynik)#zwracam formularz z wartością iloczynu w miejscu 'wynik'; nie rozumiem czemu muszę napisać wynik=wynik
-'''
-@app.route('/', methods=['GET', 'POST']) 
-def currency():#nie wiem czy prawidłowo rozumiem tą funkcję:
-   if request.method == 'GET': #jeśli zapytanie request to get
-       return render_template ("form.html")#to zwraca formularz na stronie
-   elif request.method == 'POST':#jeśli zapytanie request to post to
-       data=request.form #która wersja zapisu pobierania danych jest poprawna z get czy request.form?
-       waluta=data.get('waluta')#pobieram z formularza wartość 'waluta'
-       ilość =data.get('ilość')#pobieram z formularza wartość 'ilość'
-       get_rate('data.csv', 'waluta')#odpalam funkcje wybierającą z pliku z api wybraną w formularzu walutę
-       wynik=float(ilość)*float(waluta)#przypisuję do zmiennej 'wynik' iloczyn floatów z 'waluty' i 'ilości'
-       return render_template('form.html', wynik=wynik)#zwracam formularz z wartością iloczynu w miejscu 'wynik'; nie rozumiem czemu muszę napisać wynik=wynik
-'''
+def currency():
+   if request.method == 'GET':
+       return render_template ("form.html")
+   elif request.method == 'POST':
+       waluta=request.form['waluta']
+       ilosc =request.form['ilosc']
+       result=get_rate('data.csv', waluta)
+       wynik=float(ilosc)*result
+       return render_template('form.html', wynik=wynik)#nie rozumiem czemu muszę napisać wynik=wynik
 
 if __name__ == "__main__":
     app.run(debug=True)
